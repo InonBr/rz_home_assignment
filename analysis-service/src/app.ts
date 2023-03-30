89f6c1258576104e1baf70e34ebcc56ecb849b32
Inon Bercu <inonbr@gmail.com>
2023-03-30 13:21:43 +0300

    created getter function

--- analysis-service/src/app.ts
+++ analysis-service/src/app.ts
@@ -2,11 +2,13 @@ import express, { Express } from "express";
 import { port } from "./config";
 import { getVirusTotalDataForDomain } from "../systems/virusTotalData";
 import { getWhoisData } from "../systems/whoisData";
+import { getSslDetails } from "../systems/sslData";
 
 const app: Express = express();
 
-getVirusTotalDataForDomain("google.com");
-getWhoisData("google.com");
+// getVirusTotalDataForDomain("google.com");
+// getWhoisData("google.com");
+// getSslDetails("google.com");
 
 app.listen(port, () => {
   console.log(`🟢 App listening at http://localhost:${port}`);
