ecad299e5cffc3224e7a7c2fb8d69a099960127e
Inon Bercu <inonbr@gmail.com>
2023-03-30 14:28:31 +0300

    created rmq-package

--- analysis-service/src/app.ts
+++ analysis-service/src/app.ts
@@ -3,8 +3,11 @@ import { port } from "./config";
 import { getVirusTotalDataForDomain } from "../systems/virusTotalData";
 import { getWhoisData } from "../systems/whoisData";
 import { getSslDetails } from "../systems/sslData";
+import { receiveMsgFromQueue } from "../../rmq-package/dist";
 
 const app: Express = express();
+
+receiveMsgFromQueue;
 
 // getVirusTotalDataForDomain("google.com");
 // getWhoisData("google.com");
---
+++ rmq-package/src/index.ts
@@ -0,0 +1,78 @@
+import { connect } from "amqplib/callback_api";
+
+export const receiveMsgFromQueue = ({
+  msg,
+  queueName,
+  amqpHost,
+}: {
+  msg: any;
+  queueName: string;
+  amqpHost: string;
+}) => {
+  connect(amqpHost, (err: Error, connection) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+
+    connection.createChannel((err: Error, channel) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+
+      channel.assertQueue(queueName, {
+        durable: false,
+      });
+
+      channel.consume(
+        queueName,
+        async () => {
+          if (msg) {
+            console.debug(
+              `Received message from queue: ${msg.content.toString()}`
+            );
+            const data: any = JSON.parse(msg.content.toString());
+
+            console.log(data);
+          }
+        },
+        {
+          noAck: true,
+        }
+      );
+    });
+  });
+};
+
+export const sendDataToQueue = ({
+  msg,
+  queueName,
+  amqpHost,
+}: {
+  msg: any;
+  queueName: string;
+  amqpHost: string;
+}) => {
+  connect(amqpHost, (err: Error, connection) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+
+    connection.createChannel((err: Error, channel) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+
+      channel.assertQueue(queueName, {
+        durable: false,
+      });
+
+      console.debug(`Sending message to queue: ${JSON.stringify(msg)}`);
+
+      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(msg)));
+    });
+  });
+};
